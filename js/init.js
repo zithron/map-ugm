735892df7695e8d339a6b2600daeb9b4a462df87
zithron <abdulbasithb70@gmail.com>
2017-07-02 22:10:46 +0700

    update popover

--- js/init.js
+++ js/init.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popover"]').popover({
+        html: true,
+        placement: 'top',
+        trigger: 'focus',
+        content: function() {
+            return '<p>' + $(this).data('deskripsi') + '</p>' +
+                '<p><a>' + $(this).data('link') + '</a></p>' +
+                '<img src="' + $(this).data('img') + '" />'
+        },
+        template: '<div class="popover">' +
+            '<div class="arrow"></div>' +
+            '<i class="close fa fa-times" aria-hidden="true"></i>' +
+            '<h3 class="popover-title"></h3>' +
+            '<div class="popover-content"></div>' +
+            '</div>'
+    });
 });
+
+ // placement: function(context, source) {
+ //            var position = $(source).position();
+
+ //            if (position.left > 515) {
+ //                return "left";
+ //            }
+
+ //            if (position.left < 515) {
+ //                return "right";
+ //            }
+
+ //            if (position.top < 110) {
+ //                return "bottom";
+ //            }
+
+ //            return "top";
+ //        },
